5f15fc9ed49e8cfa778d35c38cdc4e958783c395
Amr Gad <merodiro@gmail.com>
2020-04-29 21:25:55 +0200

    auto start tmux on ssh

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,13 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+
+# auto start tmux when ssh 
+if status --is-login
+    set PPID (echo (ps --pid %self -o ppid --no-headers) | xargs)
+    if ps --pid $PPID | grep ssh
+        tmux has-session -t remote; and tmux attach-session -t remote; or tmux new-session -s remote; and kill %self
+        echo "tmux failed to start; using plain fish shell"
+    end
+end
