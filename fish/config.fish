473e511ded5e26803e835387201d79a7795a1fbe
Amr Gad <merodiro@gmail.com>
2020-11-27 18:48:42 +0200

    update fish time format

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ source ~/.bash_aliases
 set --universal fish_user_paths $fish_user_paths ~/.bin
 set -gx EDITOR nvim
 set -g theme_color_scheme "dracula"
+set -g theme_date_format "+%a %H:%M"
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
