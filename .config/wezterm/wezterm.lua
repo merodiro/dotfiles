2ca727708096806c341b73218007d00a3ef5ceb6
Amr Gad <merodiro@gmail.com>
2024-01-15 18:21:24 +0200

    Enable bidi in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,15 +6,18 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 if wezterm.gui.get_appearance():find("Dark") then
-	config.color_scheme = "Catppuccin Mocha"
+  config.color_scheme = "Catppuccin Mocha"
 else
-	config.color_scheme = "Catppuccin Latte"
+  config.color_scheme = "Catppuccin Latte"
 end
 
 config.font = wezterm.font("Iosevka Term Curly Slab")
+
+config.bidi_enabled = true
+config.bidi_direction = "LeftToRight"
 
 return config
