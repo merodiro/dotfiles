dbc66619dbefe46b64706c53b942d2897d3a5795
Amr Gad <merodiro@gmail.com>
2026-04-23 18:51:21 +0200

    install zsh in workspaces

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 set -e
 
+install_zsh() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        case "$ID" in
+            ubuntu|debian)
+                if ! command -v zsh > /dev/null; then
+                    export DEBIAN_FRONTEND=noninteractive
+                    sudo apt-get update -qq
+                    sudo apt-get install -y -qq zsh
+                fi
+                if [ "$SHELL" != "$(command -v zsh)" ]; then
+                    sudo chsh -s "$(command -v zsh)" "$USER"
+                fi
+                ;;
+        esac
+    fi
+}
+
+install_zsh
+
 # Install chezmoi and apply dotfiles
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- init --apply merodiro
 
